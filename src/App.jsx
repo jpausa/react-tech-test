39ed8c0a59166d9e263c5403a87ceb15f76b0b35
Jorge <glph8911@gmail.com>
2023-06-12 18:11:50 -0500

    adding App component, fetching APIs and painting the content

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,32 @@
+import { useEffect, useState } from 'react'
+
+const { VITE_CATS_FACTS_API_URL, VITE_CATS_IMAGES_API_URL_PREFIX, VITE_CATS_IMAGES_API_URL_SUFFIX } = import.meta.env
+
 export const App = () => {
-  return (<h1> Cats app </h1>)
+  const [fact, setFact] = useState()
+  const [imageUrl, setImageUrl] = useState()
+
+  useEffect(() => {
+    fetch(VITE_CATS_FACTS_API_URL).then(res =>
+      res.json()
+    ).then(data => {
+      const { fact } = data
+      setFact(fact)
+
+      const threeFirstWords = fact?.split(' ', 3).join(' ')
+
+      fetch(`${VITE_CATS_IMAGES_API_URL_PREFIX}${VITE_CATS_IMAGES_API_URL_SUFFIX}${threeFirstWords}?size=50&color=red&json=true`).then(res => res.json()).then(data => {
+        const { url } = data
+        setImageUrl(url)
+      })
+    })
+  }, [])
+
+  return (
+    <main>
+      <h1> Cats app </h1>
+      {fact && <p>{fact}</p>}
+      {imageUrl && <img src={`${VITE_CATS_IMAGES_API_URL_PREFIX}${imageUrl}`} alt='An image of a cat from cataas.com' />}
+    </main>
+  )
 }
